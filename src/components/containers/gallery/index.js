d8ad25f386793576c9d7e99f20cd828247f2754a
emdee19 <mehmadk05@gmail.com>
2023-04-14 17:07:32 +0500

    changes in gallery page carousel remaining section courses owl carousel icons

--- src/components/containers/gallery/index.js
+++ src/components/containers/gallery/index.js
@@ -1,6 +1,6 @@
 import gallery_image1 from "/src/assets/gallery-image.png"
-import gallery_image2 from "/src/assets/slider-images/slider-1.jpg"
-import gallery_image3 from "/src/assets/slider-images/slider-2.jpg"
+import gallery_image2 from "/src/assets/slider-images/slider-2.jpg"
+import gallery_image3 from "/src/assets/slider-images/slider-3.jpg"
 
 export {
     gallery_image1,
